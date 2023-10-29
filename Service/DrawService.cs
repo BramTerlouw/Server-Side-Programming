ea8f94daa2f49085e2a8074ac8216667a350a8a6
BramTerlouw <bramterlouw12@gmail.com>
2023-10-29 22:41:02 +0100

    small change

--- Service/DrawService.cs
+++ Service/DrawService.cs
@@ -14,7 +14,7 @@ namespace JobQueueTrigger.Service
             image.Settings.FillColor        = MagickColors.Black;
             image.Settings.BorderColor      = MagickColors.Black;
             image.Settings.FontWeight       = FontWeight.Bold;
-            image.Settings.FontPointsize    = 12;
+            image.Settings.FontPointsize    = 20;
 
             DrawableText stationName    = new DrawableText(50, 100, $"Station: {measurement.stationname}");
             DrawableText temperature    = new DrawableText(50, 130, $"Temperature: {measurement.temperature}");
